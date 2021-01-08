4b01bb7eee5c68675ada0d56cbfbb525081fcf04
Steven Sarria <ssarr012@fiu.edu>
2021-01-08 18:35:33 -0500

    added URI code

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //require everything we're gonna need//
-
 const express = require('express');
 const cors = require('cors');
+
+//what helps us connect to our mongoDB database//
+const mongoose = require('mongoose');
 
 //configures so we can have our environment variables in the .env file//
 require('dotenv').config();
@@ -13,6 +15,10 @@ const port = process.env.port || 5000;
 //middleware - express.js allows us to pars json because our server lets us send and receive json//
 app.use(cors());
 app.use(express.json);
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
 
 //what starts the server//
 app.listen(port, () => {
